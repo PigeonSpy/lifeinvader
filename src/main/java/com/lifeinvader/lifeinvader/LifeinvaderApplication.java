b6bfa6551ce6f56bef678d0d77c8120c27f8b2c3
Pijus Lucinskas <plucinskas1@sheffield.ac.uk>
2025-01-08 12:32:27 +0000

    Initial commit.

---
+++ src/main/java/com/lifeinvader/lifeinvader/LifeinvaderApplication.java
@@ -0,0 +1,13 @@
+package com.lifeinvader.lifeinvader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeinvaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeinvaderApplication.class, args);
+	}
+
+}
